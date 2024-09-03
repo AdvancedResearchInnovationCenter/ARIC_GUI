c5a30dfd2fee4afea1db36291660893535f03ffd
AmalAlkaabi <amalalkaabi677@gmail.com>
2024-09-03 16:39:11 +0400

    Delete amalalkaabi_ws/src directory

--- amalalkaabi_ws/src/trial.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rosclean purge -y
-
- 
-gnome-terminal -- /bin/bash  -c 'conda init; source /home/dark/miniconda3/etc/profile.d/conda.sh; conda activate py27;xdg-open gui.html'
-
-xdg-open gui.html
-
